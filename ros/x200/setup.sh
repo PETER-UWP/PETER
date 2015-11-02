fd7816765c1e0262abb86839fe8216f350bb8dca
unDeadHerbs <undeadherbs@gmail.com>
2015-11-02 12:47:10 -0600

    fixed some typos, also added initial instructions

--- ros/x200/setup.sh
+++ ros/x200/setup.sh
@@ -1,7 +1,15 @@
+#apt-get install curl -y
+#curl https://raw.githubusercontent.com/PETER-UWP/PETER/master/ros/x200/setup.sh|sh
+
 ## setup debian settings
 sed -e 's/^GRUB_TIMEOUT[=].*$/GRUB_TIMEOUT=0/' /etc/default/grub > /etc/default/grub1
 mv /etc/default/grub1 /etc/default/grub
 update-grub&
+
+## wifi drivers for the x200
+# commented out becoues UWP has non-standard wifi protocals
+# apt-get update && apt-get install firmware-iwlwifi
+# modprobe -r iwlwifi ; modprobe iwlwifi
 
 ## setup ros
 sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
@@ -18,5 +26,5 @@ apt-get install python-rosinstall -y
 ## setup ros's atachment to git
 su ros -c 'echo "sh ~/rosinit.sh">>~/.bashrc'
 su ros -c 'mkdir ~/git'
-su ros -c '(cd ~/git/ && git clone http://github.com:PETER-UWP/PETER.git)'
+su ros -c '(cd ~/git/ && git clone http://github.com/PETER-UWP/PETER.git)'
 su ros -c 'sh ~/git/PETER/ros/x200/startup.sh'
