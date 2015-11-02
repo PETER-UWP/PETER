19fb84f25100b8a0015806ce489b64e97588a90b
unDeadHerbs <undeadherbs@gmail.com>
2015-11-01 23:46:02 -0600

    created an automatic way of reinitializing ros

---
+++ ros/x200/setup.sh
@@ -0,0 +1,9 @@
+sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
+apt-get update
+apt-get install xserver-xorg-dev-lts-utopic mesa-common-dev-lts-utopic libxatracker-dev-lts-utopic libopenvg1-mesa-dev-lts-utopic libgles2-mesa-dev-lts-utopic libgles1-mesa-dev-lts-utopic libgl1-mesa-dev-lts-utopic libgbm-dev-lts-utopic libegl1-mesa-dev-lts-utopic ros-indigo-desktop-full aptitude -y
+rosdep init
+adduser ros
+su ros -c 'rosdep update'
+su ros -c 'echo "source /opt/ros/indigo/setup.bash" >> ~/.bashrc'
+apt-get install python-rosinstall -y
