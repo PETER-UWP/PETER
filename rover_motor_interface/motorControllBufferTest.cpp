27bdb31b1f325de89fece39f005b47c408cb9cb1
unDeadHerbs <undeadherbs@gmail.com>
2015-11-13 23:19:07 -0600

    made the motor controller test better

--- rover_motor_interface/motorControllBufferTest.cpp
+++ rover_motor_interface/motorControllBufferTest.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "motorControllBuffer.hpp"
+#include <unistd.h>
 
 int main(){
 	moterController weels("/dev/serial/by-id/usb-Arduino_LLC_Arduino_Leonardo-if00");
@@ -7,5 +8,12 @@ int main(){
 	if (!weels.areConnected()){
 		std::cout<<"Failed to open!"<<std::endl;
 	}
+	moterController.move(10,10);
+	sleep(3);
+	moterController.move(10,-10);
+	sleep(3);
+	moterController.move(-10,10);
+	sleep(3);
+	moterController.move(-10,-10);
 	return 0;
 }
